63de213f39a4cfe0eceef7642a8445c874795de3
vdymir <vladistar200302@gmail.com>
2026-01-17 17:11:23 -0500

    Update app configuration and add custom fonts
    - Removed unused Android adaptive icon images from app.json.
    - Added custom fonts (RedHatDisplay and Poppins) to the project and integrated them into the RootLayout.
    - Updated layout to handle font loading and splash screen management.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,36 @@
 import { COLORS } from '@/src/shared/colors';
 import {
   QueryClient,
-  QueryClientProvider,
+  QueryClientProvider
 } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "RedHatDisplay-Bold": require("../assets/fonts/RedHatDisplay-Bold.ttf"),
+    "RedHatDisplay-SemiBold": require("../assets/fonts/RedHatDisplay-SemiBold.ttf"),
+    "RedHatDisplay-Black": require("../assets/fonts/RedHatDisplay-Black.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: COLORS.primary } }}>
